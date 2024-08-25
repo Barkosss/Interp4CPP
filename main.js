118d151c77a6887f0d289416de93197feea08989
Andrey Baryshev <andrey.bar66@gmail.com>
2024-08-25 09:03:05 +0500

    Update

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 const fs = require('fs');
+import func from 'functions.js';
 
-// 4Мб данных = 33554432 байт
+// 4Mb data = 33554432 byte
 let memory = new Array(4 * 1024 * 1024 * 8).fill(0);
 
+// ...
+let userProgramFile = process.argv[2];
+
+// ...
+if (!userProgramFile) {
+    throw new Error("file not found");
+}
+
+// ...
+let codeFromUserProgramFile = fs.readFileSync(userProgramFile, 'utf-8').split("\n");
+
+console.log(codeFromUserProgramFile);
